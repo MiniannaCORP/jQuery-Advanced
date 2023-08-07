5e6351e220cfbbc5c3186289807beeaab2271272
ycarbajal <yoelcarbajalmingol@outlook.com.pe>
2023-08-07 15:12:05 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,1 @@
+document.querySelector('#app').innerHTML = `<h4>add main.js</h4>`
---
+++ public/01-start/base.js
@@ -0,0 +1,25 @@
+import $ from "jquery";
+
+$(() => {
+  let route = {
+    _routes: {}
+    , add: function( url, action ) {
+      this._routes[url] = action
+    }
+    , run: function() {
+      $.each( this._routes, function( patron ) {
+        if ( location.href.match( patron ) ) {
+          this();
+        }
+      });
+    }
+  }
+
+  route.add( 'index.html', function(){ console.log('Este código, solo funciona en el primer parámetro'); });
+  route.add( 'about.html', function(){ console.log('Este código, solo funciona en about.html'); });
+  route.add( '.*html', function(){ console.log('Este código, solo funciona en cualquier página html'); });
+
+  
+
+  route.run();
+});
